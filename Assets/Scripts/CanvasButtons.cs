d5ab7f1ea6b564c7359257ddcb29c2c188e399a1
Tancik00 <ivancioglu.tania@gmail.com>
2020-12-06 18:47:46 +0200

    Added UI for game starting

---
+++ Assets/Scripts/CanvasButtons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CanvasButtons : MonoBehaviour
+{
+    public GameObject panel;
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        panel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
